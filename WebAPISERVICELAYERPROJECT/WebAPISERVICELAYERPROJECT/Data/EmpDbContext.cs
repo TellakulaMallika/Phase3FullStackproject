24daf8e847e067fa107fd3c52db678c37f60769a
T.Mallika <tellakulamallika@gmailc.om>
2024-02-10 18:33:52 -0700

    firstcommit

---
+++ WebAPISERVICELAYERPROJECT/WebAPISERVICELAYERPROJECT/Data/EmpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPISERVICELAYERPROJECT.Models;
+
+namespace WebAPISERVICELAYERPROJECT.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext (DbContextOptions<EmpDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPISERVICELAYERPROJECT.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
---
+++ WebAPISERVICELAYERPROJECT/WebAPISERVICELAYERPROJECT/Models/EmpProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPISERVICELAYERPROJECT.Models
+{
+    [Table("EmpProfile")]
+    public class EmpProfile
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public string? EmpName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string? Email { get; set; }
+        public int DeptCode { get; set; }
+    }
+}
+
